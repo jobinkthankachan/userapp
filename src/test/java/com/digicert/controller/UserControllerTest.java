3a997dac64c7242b04855c746cf6a1a1b29ec623
Jobin <jobint@nsfas.org.za>
2023-07-16 17:10:02 +0200

    Log added

--- src/test/java/com/digicert/controller/UserControllerTest.java
+++ src/test/java/com/digicert/controller/UserControllerTest.java
@@ -5,11 +5,12 @@ import com.digicert.exception.UserNotFoundException;
 import com.digicert.model.User;
 import com.digicert.service.UserService;
 import com.fasterxml.jackson.core.JsonProcessingException;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -25,8 +26,10 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserControllerTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserControllerTest.class);
     private UserController userController;
     private UserService userService;
 
@@ -38,7 +41,9 @@ class UserControllerTest {
 
 
     @Test
+    @Order(1)
     void getAllUsers() throws JsonProcessingException {
+        logger.info("***** Get All User Test Started *****");
         List<UserDTO> userList = new ArrayList<>();
         userList.add(new UserDTO(1L, "Test", "Test", "test1@gmail.com"));
         userList.add(new UserDTO(2L, "Test", "Test", "test2@gmail.com"));
@@ -59,10 +64,13 @@ class UserControllerTest {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
+        logger.info("***** Get All User Test Completed *****");
     }
 
     @Test
+    @Order(2)
     void getUserById_whenUserExist_thenReturnUser() throws Exception {
+        logger.info("***** getUserById_whenUserExist_thenReturnUser Test Started *****");
         UserDTO user = new UserDTO(1L, "Test1", "Test1", "test1@gmail.com");
         when(userService.getUserById(1L)).thenReturn(java.util.Optional.of(user));
 
@@ -75,10 +83,13 @@ class UserControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(expectedJson));
 
+        logger.info("***** getUserById_whenUserExist_thenReturnUser Test Completed *****");
     }
 
     @Test
+    @Order(3)
     void getUserById_whenUserNotExist_thenReturnUserNotFound() throws Exception {
+        logger.info("***** getUserById_whenUserNotExist_thenReturnUserNotFound Test Started *****");
         Long userId =1L;
         when(userService.getUserById(userId)).thenReturn(java.util.Optional.empty());
 
@@ -97,11 +108,14 @@ class UserControllerTest {
                 throw e;
             }
         }
+        logger.info("***** getUserById_whenUserNotExist_thenReturnUserNotFound Test Completed *****");
     }
 
 
     @Test
+    @Order(4)
     void createUser() throws Exception {
+        logger.info("***** createUser Test Started *****");
         UserDTO user = new UserDTO(1L,"Test1","Test1","test1@gmail.com");
         when(userService.createUser(any(UserDTO.class))).thenReturn(user);
 
@@ -115,11 +129,13 @@ class UserControllerTest {
                         .content(requestJson))
                 .andExpect(MockMvcResultMatchers.status().isCreated())
                 .andExpect(MockMvcResultMatchers.content().json(requestJson));
-
+        logger.info("***** createUser Test Completed *****");
     }
 
     @Test
+    @Order(5)
     void updateUser_whenUserExist_thenReturnOk() throws Exception {
+        logger.info("***** updateUser_whenUserExist_thenReturnOk Test Started *****");
         UserDTO user = new UserDTO(1L, "Test", "Test", "test1@gmail.com");
         when(userService.updateUser(eq(1L), any(UserDTO.class))).thenReturn(user);
 
@@ -133,11 +149,13 @@ class UserControllerTest {
                         .content(requestJson))
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.content().json(requestJson));
-
+        logger.info("***** updateUser_whenUserExist_thenReturnOk Test Completed *****");
     }
 
     @Test
+    @Order(6)
     void updateUser_whenUserNotExist_thenReturnUserNotFound() throws Exception {
+        logger.info("***** updateUser_whenUserNotExist_thenReturnUserNotFound Test Started *****");
         Long userId = 1L;
         UserDTO user = new UserDTO(userId, "Test1", "Test1", "test1@gmail.com");
         when(userService.updateUser(eq(userId), any(UserDTO.class))).thenThrow(UserNotFoundException.class);
@@ -162,21 +180,25 @@ class UserControllerTest {
                 throw e;
             }
         }
+        logger.info("***** updateUser_whenUserNotExist_thenReturnUserNotFound Test Completed *****");
     }
 
     @Test
+    @Order(7)
     void deleteUser_whenUserExist_thenReturnNothing() throws Exception {
-
+        logger.info("***** deleteUser_whenUserExist_thenReturnNothing Test Started *****");
         MockMvc mockMvc = MockMvcBuilders.standaloneSetup(userController).build();
         //when there is a user that exist to delete
         mockMvc.perform(MockMvcRequestBuilders.delete("/users/1"))
                 .andExpect(MockMvcResultMatchers.status().isNoContent());
 
-
+        logger.info("***** deleteUser_whenUserExist_thenReturnNothing Test Completed *****");
     }
 
     @Test
+    @Order(8)
     void deleteUser_whenUserNotExist_thenReturnUserNotFound() throws Exception {
+        logger.info("***** deleteUser_whenUserNotExist_thenReturnUserNotFound Test Started *****");
         //when user doesnt exist to delete , throw user not found exception
         Long userId = 1L;
         Mockito.doThrow(UserNotFoundException.class).when(userService).deleteUser(userId);
@@ -193,5 +215,6 @@ class UserControllerTest {
                     throw e;
                 }
             }
+        logger.info("***** deleteUser_whenUserNotExist_thenReturnUserNotFound Test Completed *****");
     }
 }
